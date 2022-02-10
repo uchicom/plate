475cd3538e4e4ec40b1349bef962f324f8d9b489
uchicom <shigeki.uchiyama@uchicom.com>
2022-02-10 23:32:11 +0000

    #1 cron設定追加

--- src/main/java/com/uchicom/plate/scheduler/cron/Cron.java
+++ src/main/java/com/uchicom/plate/scheduler/cron/Cron.java
@@ -2,21 +2,30 @@
 package com.uchicom.plate.scheduler.cron;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import com.uchicom.util.Numbre;
 
 public class Cron {
   // enum EVERY="*",  ARRAY(1,3 or 1-3)
-  private static final String EVERY = "*";
   String minute;
   String hour;
   String day;
   String month;
   String dayOfWeek;
   String command;
+  int[] months;
+  int[] days;
+  int[] hours;
+  int[] minutes;
 
-  int[] trigger;
+  int[] triggers;
   int scheduledTriggerIndex = -1;
   // strategyを用意して検索する
 
+  public Cron() {}
   public Cron(String[] description) {
     minute = description[0];
     hour = description[1];
@@ -24,14 +33,83 @@ public class Cron {
     month = description[3];
     dayOfWeek = description[4];
     command = description[5];
-    init();
+    initTriggers(minute,hour,day,month,dayOfWeek,command);
+    setScheduledTriggerIndex();
   }
 
-  void init() {
-    // 作成時にint[]で01010000 1/1 0:0などを保持
+  int[] parse(String pattern) {
+    List<Integer> expandList = Numbre.expand(pattern);
+    return expandList.stream().mapToInt(Integer::intValue).sorted().toArray();
+  }
 
-    // TODO 2分法で起動対象を検索して最初のtriggerIndexを確定する
-    int nibunIndex = nibun(); // nowをもとに起動するindexの1つ前を設定する
+  int[] createNumbers(int start, int end) {
+    int[] numbers = new int[end - start + 1];
+    for (int i = 0; i < end - start + 1; i++) {
+      numbers[i] = start + i;
+    }
+    return numbers;
+  }
+
+  int[] getNumbers(String condition, int start, int end) {
+    if (condition == "*") {
+      return createNumbers(start, end);
+    } else {
+      return parse(condition);
+    }
+  }
+
+  int createTrigger(int month, int day, int hour, int minute) {
+    return month * 100_00_00 + day * 100_00 + hour * 100 + minute;
+  }
+
+  void setScheduledTriggerIndex() {
+    LocalDateTime now = LocalDateTime.now();
+    int nowTrigger = createTrigger(now.getMonthValue(), now.getDayOfMonth(), now.getHour(), now.getMinute());
+    scheduledTriggerIndex = getIndexByNibun(nowTrigger, 0, triggers.length);
+  }
+  int getIndexByNibun(int now, int start, int length) {
+    if (length == 1) {
+      if (triggers[start] <= now) {
+        return start;
+      }
+      if (start == 0) {
+        return triggers.length - 1;
+      } else {
+        return start -1;
+      }
+    }
+    int index = start + length / 2;
+    if (triggers[index] == now) {
+      return index;
+    } else if (triggers[index] > now) {
+      return getIndexByNibun(now, start, index - start);
+    } else {
+      int nextLength = length - (index + 1 - start);
+      if (nextLength == 0) {
+        return index;
+      }
+      return getIndexByNibun(now, index + 1, nextLength);
+    }
+  }
+  void initTriggers(String minute, String hour, String day, String month, String dayOfWeek, String command) {
+    // 作成時にint[]で01010000 1/1 0:0などを保持
+    months = getNumbers(month, 1, 12);
+    days = getNumbers(day, 1, 31);
+    hours = getNumbers(hour, 0, 23);
+    minutes = getNumbers(minute, 0, 59);
+    triggers =
+        IntStream.of(months)
+            .flatMap(
+                m ->
+                    IntStream.of(days)
+                        .flatMap(
+                            d ->
+                                IntStream.of(hours)
+                                    .flatMap(
+                                        h ->
+                                            IntStream.of(minutes)
+                                                .map(mi -> createTrigger(m, d, h, mi))))).toArray();
+
   }
 
   int nibun() {
